b880c89f3caef0640c4a634c18cbc027033edb62
jungpaeng <meis1541@naver.com>
2019-03-26 23:47:36 +0900

    :sparkles: Create Server Listening

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import * as express from 'express'
 const app = express()
 
-app.get('/', (req: express.Request, res: express.Response) => {
-  res.send('Hello World!')
-})
+const PORT = 3000
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!')
-})
+const handleListening = () => {
+  console.log('Listening on port 3000!')
+}
+
+app.listen(PORT, handleListening)
