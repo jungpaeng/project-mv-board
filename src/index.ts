c40e6706854037f91e9b37a795dd88ee7def5b5f
jungpaeng <meis1541@naver.com>
2019-03-27 22:06:58 +0900

    :sparkles: Apply Express Middleware

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 import * as express from 'express'
+import * as morgan from 'morgan'
+import * as helmet from 'helmet'
+import * as cookieParser from 'cookie-parser'
+import * as bodyParser from 'body-parser'
+
 const app = express()
 
 const PORT = 3000
@@ -10,6 +15,12 @@ const handleListening = () => {
 const handleHome = (req: express.Request, res: express.Response) => {
   res.send('Home')
 }
+
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(helmet())
+app.use(morgan('dev'))
 
 app.get('/', handleHome)
 
