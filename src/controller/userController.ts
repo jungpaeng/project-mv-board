9299608bf85899d5ae849e0446d335b35114c9b8
jungpaeng <meis1541@naver.com>
2019-03-29 20:25:27 +0900

    :sparkles: Create Template Variables in Pug

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -1,9 +1,16 @@
 import * as express from 'express'
 
-export const join = (req: express.Request, res: express.Response) => res.render('join')
-export const login = (req: express.Request, res: express.Response) => res.render('login')
-export const logout = (req: express.Request, res: express.Response) => res.render('logout')
-export const user = (req: express.Request, res: express.Response) => res.render('user')
-export const userDetail = (req: express.Request, res: express.Response) => res.render('userDetail')
-export const editProfile = (req: express.Request, res: express.Response) => res.render('editProfile')
-export const changePassword = (req: express.Request, res: express.Response) => res.render('changePassword')
+export const join = (req: express.Request, res: express.Response) =>
+  res.render('join', { pageTitle: 'Join' })
+export const login = (req: express.Request, res: express.Response) =>
+  res.render('login', { pageTitle: 'Login' })
+export const logout = (req: express.Request, res: express.Response) =>
+  res.render('logout', { pageTitle: 'Logout' })
+export const user = (req: express.Request, res: express.Response) =>
+  res.render('user', { pageTitle: 'User' })
+export const userDetail = (req: express.Request, res: express.Response) =>
+  res.render('userDetail', { pageTitle: 'User Detail' })
+export const editProfile = (req: express.Request, res: express.Response) =>
+  res.render('editProfile', { pageTitle: 'Edit Profile' })
+export const changePassword = (req: express.Request, res: express.Response) =>
+  res.render('changePassword', { pageTitle: 'Change Password' })
--- src/controller/videoController.ts
+++ src/controller/videoController.ts
@@ -1,9 +1,16 @@
 import * as express from 'express'
 
-export const root = (req: express.Request, res: express.Response) => res.render('root')
-export const search = (req: express.Request, res: express.Response) => res.send('search')
-export const video = (req: express.Request, res: express.Response) => res.send('video')
-export const upload = (req: express.Request, res: express.Response) => res.send('upload')
-export const videoDetail = (req: express.Request, res: express.Response) => res.send('videoDetail')
-export const editVideo = (req: express.Request, res: express.Response) => res.send('editVideo')
-export const deleteVideo = (req: express.Request, res: express.Response) => res.send('deleteVideo')
+export const root = (req: express.Request, res: express.Response) =>
+  res.render('root', { pageTitle: 'Home' })
+export const search = (req: express.Request, res: express.Response) =>
+  res.render('search', { pageTitle: 'Search' })
+export const video = (req: express.Request, res: express.Response) =>
+  res.render('video', { pageTitle: 'Video' })
+export const upload = (req: express.Request, res: express.Response) =>
+  res.render('upload', { pageTitle: 'Upload' })
+export const videoDetail = (req: express.Request, res: express.Response) =>
+  res.render('videoDetail', { pageTitle: 'Video Detail' })
+export const editVideo = (req: express.Request, res: express.Response) =>
+  res.render('editVideo', { pageTitle: 'Edit Video' })
+export const deleteVideo = (req: express.Request, res: express.Response) =>
+  res.render('deleteVideo', { pageTitle: 'Delete Video' })
