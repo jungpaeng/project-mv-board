5473b19ff76dd644adf0a93d14c90d764adb51be
jungpaeng <meis1541@naver.com>
2019-03-29 20:39:57 +0900

    :sparkles: Create Search Controller

--- src/controller/videoController.ts
+++ src/controller/videoController.ts
@@ -2,15 +2,30 @@ import * as express from 'express'
 
 export const root = (req: express.Request, res: express.Response) =>
   res.render('root', { pageTitle: 'Home' })
-export const search = (req: express.Request, res: express.Response) =>
-  res.render('search', { pageTitle: 'Search' })
+
+export const search = (req: express.Request, res: express.Response) => {
+  const { query: { term: searchingBy } } = req
+
+  res.render(
+    'search',
+    {
+      pageTitle: 'Search',
+      searchingBy
+    }
+  )
+}
+
 export const video = (req: express.Request, res: express.Response) =>
   res.render('video', { pageTitle: 'Video' })
+
 export const upload = (req: express.Request, res: express.Response) =>
   res.render('upload', { pageTitle: 'Upload' })
+
 export const videoDetail = (req: express.Request, res: express.Response) =>
   res.render('videoDetail', { pageTitle: 'Video Detail' })
+
 export const editVideo = (req: express.Request, res: express.Response) =>
   res.render('editVideo', { pageTitle: 'Edit Video' })
+
 export const deleteVideo = (req: express.Request, res: express.Response) =>
   res.render('deleteVideo', { pageTitle: 'Delete Video' })
