ee7ef77ce418c17fee852f6164767766d65f0e6c
jungpaeng <meis1541@naver.com>
2019-03-30 21:28:29 +0900

    :sparkles: Create Join Controller

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -1,16 +1,38 @@
 import * as express from 'express'
+import route from '../constant/route'
 
-export const join = (req: express.Request, res: express.Response) =>
+export const getJoin = (req: express.Request, res: express.Response) =>
   res.render('join', { pageTitle: 'Join' })
+
+export const postJoin = (req: express.Request, res: express.Response) => {
+  const {
+    body: { name, email, password, password2 }
+  } = req
+
+  if (password !== password2) {
+    res.status(400)
+    res.render('join', { pageTitle: 'Join' })
+  } else {
+    // TODO: Register User
+    // TODO: Log user in
+    res.redirect(route.root)
+  }
+}
+
 export const login = (req: express.Request, res: express.Response) =>
   res.render('login', { pageTitle: 'Login' })
+
 export const logout = (req: express.Request, res: express.Response) =>
   res.render('logout', { pageTitle: 'Logout' })
+
 export const user = (req: express.Request, res: express.Response) =>
   res.render('user', { pageTitle: 'User' })
+
 export const userDetail = (req: express.Request, res: express.Response) =>
   res.render('userDetail', { pageTitle: 'User Detail' })
+
 export const editProfile = (req: express.Request, res: express.Response) =>
   res.render('editProfile', { pageTitle: 'Edit Profile' })
+
 export const changePassword = (req: express.Request, res: express.Response) =>
   res.render('changePassword', { pageTitle: 'Change Password' })
--- src/router/rootRouter.ts
+++ src/router/rootRouter.ts
@@ -1,13 +1,14 @@
 import * as express from 'express'
 import route from '../constant/route'
 import { root, search } from '../controller/videoController'
-import { join, login, logout } from '../controller/userController'
+import { getJoin, postJoin, login, logout } from '../controller/userController'
 
 const rootRouter = express.Router()
 
 rootRouter.get(route.root, root)
 rootRouter.get(route.search, search)
-rootRouter.get(route.join, join)
+rootRouter.get(route.join, getJoin)
+rootRouter.post(route.join, postJoin)
 rootRouter.get(route.login, login)
 rootRouter.get(route.logout, logout)
 
