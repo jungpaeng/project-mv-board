7651e6f2ad5359269009a0d6635fbc62dadc11c2
jungpaeng <meis1541@naver.com>
2019-03-30 21:48:46 +0900

    :sparkles: Create User Temp Data

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,5 +4,9 @@ import route from './constant/route'
 export const localsMiddleware = (req: express.Request, res: express.Response, next: express.NextFunction) => {
   res.locals.siteName = 'Movie Board'
   res.locals.route = route
+  res.locals.user = {
+    isAuthenticated: true,
+    id: 1
+  }
   next()
 }
