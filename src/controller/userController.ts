14ed59d5f696bccdbdd9dc6dd10c2aade9730159
jungpaeng <meis1541@naver.com>
2019-04-13 23:51:54 +0900

    :pencil: Logout Typo

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -42,7 +42,7 @@ export const postLogin = passport.authenticate('local', {
 })
 
 export const logout = (req: express.Request, res: express.Response) => {
-  req.logOut()
+  req.logout()
   res.redirect(route.root)
 }
 
