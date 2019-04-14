df0d42bd341a762bcb47822bf75e1f1de8310b54
jungpaeng <meis1541@naver.com>
2019-04-14 23:23:01 +0900

    :sparkles: Change Text at Video Play

--- assets/ts/videoPlayer.ts
+++ assets/ts/videoPlayer.ts
@@ -5,8 +5,10 @@ const playBtn: HTMLElement = document.getElementById('jsPlayButton')
 const handlePlayClick = () => {
   if (videoPlayer.paused) {
     videoPlayer.play()
+    playBtn.innerHTML = '<i class="fas fa-pause"/>'
   } else {
     videoPlayer.pause()
+    playBtn.innerHTML = '<i class="fas fa-play"/>'
   }
 }
 
